d1216fdc99c84ee14cebc504a61179f6af001bdd
jpjp1452 <jpkleijkers@outlook.com>
2025-05-06 15:27:04 +0200

    imported kaggle data set

---
+++ data_preprocessing.py
@@ -0,0 +1,45 @@
+def load_and_preprocess_data(csv_path="housing.csv",targetColumn = 'median_house_value', test_size=0.8, random_state=42,columnsToIgnore = []):
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import StandardScaler
+
+    # Charger le CSV
+    df = pd.read_csv(csv_path)
+    print("Columns:", df.columns)
+
+    #Chose target and ignore what user wants
+    A = df.drop(columns=columnsToIgnore + [targetColumn])
+    b = df[targetColumn].values
+
+    #save this to go back to unnormalized values
+    b_mean = b.mean()
+    b_std = b.std()
+
+    #replace categorical variables by booleans
+    A = pd.get_dummies(A)
+
+    # Normalization
+    scaler = StandardScaler()
+    A = scaler.fit_transform(A)
+    b = (b - b.mean()) / b.std()
+
+    # Train/test split
+    A_train, A_test, b_train, b_test = train_test_split(
+        A, b, test_size=test_size, random_state=random_state
+    )
+
+    return A_train, A_test, b_train, b_test, b_mean, b_std
+
+
+
+
+def load_and_preprocess_Housing_data(test_size=0.8, random_state=42):
+    
+    return load_and_preprocess_data(csv_path="housing.csv",targetColumn = 'median_house_value',test_size=test_size, random_state=random_state)
+
+def load_and_preprocess_Student_Performance_data(test_size=0.8, random_state=42):
+    
+    return load_and_preprocess_data(csv_path="Student_Performance.csv",targetColumn = 'Performance Index',test_size=test_size, random_state=random_state)
+
+def load_and_preprocess_Insurance_data(test_size=0.8, random_state=42):
+    return load_and_preprocess_data(csv_path="insurance.csv",targetColumn = 'charges',test_size=test_size, random_state=random_state)
--- opti.py
+++ opti.py
@@ -1,16 +1,15 @@
 import numpy as np
-from sklearn.datasets import fetch_california_housing
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import Lasso
-
-
-LASSO=0
-ELASTICNET=1
+from sklearn.datasets import fetch_california_housing
+from data_preprocessing import *
 
 
 
+"""
 # Charger les données California Housing
 data = fetch_california_housing()
 
@@ -23,9 +22,20 @@ A = scaler.fit_transform(A)
 
 # Train/test split
 A_train, A_test, b_train, b_test = train_test_split(A, b, test_size=0.2, random_state=42)
+"""
 
 
 
+A_train, A_test, b_train, b_test, b_mean, b_std=load_and_preprocess_Insurance_data()
+
+
+
+
+
+
+
+LASSO=0
+ELASTICNET=1
 def gradientCurrying(A, b):
     def g_grad(w):
         return A.T @ (A @ w - b)
@@ -33,9 +43,6 @@ def gradientCurrying(A, b):
 def soft_thresholding(x, lam):
     return np.sign(x) * np.maximum(np.abs(x) - lam, 0.0)
     
-
-
-
 
 def ISTA(A, b, lam1=0.1, MODE=LASSO, lam2=0.01, max_iter=50000, tol=1e-16, adaptive_step=False):
     if(MODE != LASSO and MODE != ELASTICNET):
@@ -65,15 +72,16 @@ def ISTA(A, b, lam1=0.1, MODE=LASSO, lam2=0.01, max_iter=50000, tol=1e-16, adapt
             x_i = x_i - t * (grad(x_i) + 2 * lam2 * x_i)
             # Proximal operator (soft-thresholding)
             x_i = soft_thresholding(x_i, lam1*t)
-        # Critère d'arrêt
+
         
         if(adaptive_step):
-            # Mise à jour de la taille du pas
+            # update step
             Lk = np.linalg.norm(grad(x_i) - grad(x_i_old), 2) / np.linalg.norm(x_i - x_i_old, 2)
             if Lk > L / 2:
                 L = Lk
                 t = 1.0 / L
         logs.append(x_i)
+        #stop criterion 
         if np.linalg.norm(x_i - x_i_old) < tol:
             break
         
@@ -91,15 +99,7 @@ def fista(A, b, lam1=0.1, MODE=LASSO, lam2=0.01, max_iter=50000, tol=1e-16):
         raise ValueError("MODE must be either LASSO or ELASTICNET")
     if(max_iter <= 0):
         raise ValueError("max_iter must be positive")
-    """
-    FISTA pour le problème LASSO: min_x 0.5*||Ax - b||^2 + lambda * ||x||_1
 
-    Paramètres :
-    A : matrice (m x n)
-    b : vecteur (m,)
-    lambda_reg : paramètre de régularisation
-    L : constante de Lipschitz de grad f (typiquement, L = plus grande valeur propre de A^T A)
-    """
     n = A.shape[1]
     x_i = np.zeros(n)
     y = x_i.copy()
@@ -109,27 +109,19 @@ def fista(A, b, lam1=0.1, MODE=LASSO, lam2=0.01, max_iter=50000, tol=1e-16):
     logs =[x_i]
     for k in range(max_iter):
         x_old = x_i.copy()
-
-
-        # Étape de mise à jour via l’opérateur proximal (soft-thresholding ici)
-        
         if(MODE==LASSO):
             x_i = soft_thresholding(y - (1/L) * (grad(y)), lam1/L)
         else:
             x_i = soft_thresholding(y - (1/L) * (grad(y)+2 * lam2 *y), lam1/L)
             
-
-
-        # Accélération de Nesterov
         t_new = (1 + np.sqrt(1 + 4 * t**2)) / 2
         y = x_i + ((t - 1) / t_new) * (x_i - x_old)
         t = t_new
 
         logs.append(x_i)
-        # Critère d'arrêt
+        #stop criterion 
         if np.linalg.norm(x_i - x_old) < tol:
             break
-
     return x_i,logs
 
 
@@ -143,25 +135,24 @@ def gradient_descent(A, b,lam=0.00001, max_iter=10000, tol=1e-16):
     for k in range(max_iter):
         x_old = x_i.copy()
         
-        # Descente de gradient
+        # gradient step
         x_i = x_i - lam * grad(x_i)
         
         logs.append(x_i.copy())
-        
-        # Critère d'arrêt
+        #stop criterion 
         if np.linalg.norm(x_i - x_old) < tol:
             break
 
     return x_i,logs
 
 
-# Initialisation
+# Initialization
 x0 = np.zeros(A_train.shape[1])
 L = np.linalg.norm(A_train.T @ A_train, 2)
 t = 1.0 / L  
 lam1 = 0.1
 lam2 = 0.01
-CURRENT_MODE = LASSO  # Choisir entre LASSO et ELASTICNET
+CURRENT_MODE = LASSO # Choose between LASSO et ELASTICNET
 
 # Exécution ISTA
 x_hat_ista, logs_ista = ISTA(A_train, b_train, lam1=lam1, MODE=CURRENT_MODE, lam2=lam2, max_iter=10000, tol=1e-16)
@@ -206,14 +197,14 @@ print(f"MSE : {mse_test:.4f}")
 
 y_pred_fista = A_train @ x_hat_fista
 mse_test_fista = mean_squared_error(b_train, y_pred_fista)
-print("==> Résultats du FISTA")
+print("==> Résultats du FISTA" )
 print(f"MSE : {mse_test_fista:.4f}")
 
 
 # Lasso sklearn : alpha = lam / n_samples
 alpha = lam1 / A_train.shape[0]
 
-model = Lasso(alpha=alpha, fit_intercept=False, max_iter=10000)
+model = Lasso(alpha=alpha, fit_intercept=False, max_iter=100000)
 model.fit(A_train, b_train)
 
 # Comparaison des résultats
@@ -227,5 +218,50 @@ print(f"MSE FISTA : {mse_test_fista:.4f}")
 print(f"Différence ISTA vs sklearn : {mse_test - mse_sklearn}")
 print(f"Différence FISTA vs sklearn : {mse_test_fista - mse_sklearn}")
 print(f"Différence ISTA vs FISTA : {mse_test - mse_test_fista}")
+print(f"Différence nb itérations ISTA vs FISTA : {len(logs_ista) - len(logs_fista)}")
+
+
+
+
+
+
+print("-------------Non normalized value-------")
+# Prédictions ISTA
+y_pred = A_train @ x_hat_ista
+y_pred_original = y_pred * b_std + b_mean
+b_train_original = b_train * b_std + b_mean
+mse_test = mean_squared_error(b_train_original, y_pred_original)
+rmse_test = np.sqrt(mse_test)
+print("==> Résultats de l'ISTA")
+print(f"MSE : {mse_test:.2f}")
+print(f"RMSE : {rmse_test:.2f}")
+
+# Prédictions FISTA
+y_pred_fista = A_train @ x_hat_fista
+y_pred_fista_original = y_pred_fista * b_std + b_mean
+mse_test_fista = mean_squared_error(b_train_original, y_pred_fista_original)
+rmse_test_fista = np.sqrt(mse_test_fista)
+print("==> Résultats du FISTA" )
+print(f"MSE : {mse_test_fista:.2f}")
+print(f"RMSE : {rmse_test_fista:.2f}")
+
+# Sklearn Lasso
+alpha = lam1 / A_train.shape[0]
+model = Lasso(alpha=alpha, fit_intercept=False, max_iter=100000)
+model.fit(A_train, b_train)
+y_pred_sklearn = model.predict(A_train)
+y_pred_sklearn_original = y_pred_sklearn * b_std + b_mean
+mse_sklearn = mean_squared_error(b_train_original, y_pred_sklearn_original)
+rmse_sklearn = np.sqrt(mse_sklearn)
+
+# Comparaison
+print("==> Résultats de comparaison")
+print(f"MSE sklearn : {mse_sklearn:.2f}")
+print(f"RMSE sklearn : {rmse_sklearn:.2f}")
+print(f"MSE ISTA : {mse_test:.2f}")
+print(f"MSE FISTA : {mse_test_fista:.2f}")
+print(f"Différence ISTA vs sklearn : {mse_test - mse_sklearn:.2f}")
+print(f"Différence FISTA vs sklearn : {mse_test_fista - mse_sklearn:.2f}")
+print(f"Différence ISTA vs FISTA : {mse_test - mse_test_fista:.2f}")
 print(f"Différence nb itérations ISTA vs FISTA : {len(logs_ista) - len(logs_fista)}")
 
